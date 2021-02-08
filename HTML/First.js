327e39771b6e08a7bcf345bf7af6591f1ee628b4
vinud03 <66354380+vinud03@users.noreply.github.com>
2021-02-08 19:46:54 +0530

    On 8 feb 2021

---
+++ HTML/First.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    // document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo").innerHTML = 5 + 8
+}
+
+// Display some data from the object:
+function details() {
+    var person = {
+        firstName: "John",
+        lastName: "Doe",
+        id: 5566,
+        fullName: function () {
+            return this.firstName + " " + this.lastName;
+        }
+    };
+    document.getElementById("demo").innerHTML = fullName()
+}
